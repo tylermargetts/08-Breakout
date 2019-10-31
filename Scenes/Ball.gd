d38ff734f7be167cbcf7d49542dec16f0ba0da01
Tyler Margetts <tmargett@iu.edu>
2019-10-30 22:02:59 -0400

    Update Ball.gd

--- Scenes/Ball.gd
+++ Scenes/Ball.gd
@@ -1,28 +1,26 @@
 extends RigidBody2D
 
+export var maxspeed = 300
+
+signal lives
+signal score
+
 func _ready():
-    contact_monitor = true
-    set_max_contacts_reported(4)
+ contact_monitor = true
+ set_max_contacts_reported(4)
+ var WorldNode = get_node("/root/World")
+ connect("score", WorldNode, "increase_score")
+ connect("lives", WorldNode, "decrease_lives")
 
 func _physics_process(delta):
-    var bodies = get_colliding_bodies()
-    for body in bodies:
-        if body.is_in_group("Tiles"):
-            body.queue_free()
+ var bodies = get_colliding_bodies()
+ for body in bodies:
+  if body.is_in_group("Tiles"):
+   emit_signal("score",body.score)
+   body.queue_free()
+  if body.get_name() == "Paddle":
+   pass
 
-func _update():
-	var lives = 3
-	var points = 0
-	$LabelLives.set_text(lives)
-	$LabelPoints.set_text(points)
-	var bodies = get_colliding_bodies()
-	for body in bodies:
-		if body.is_in_group("Tiles"):
-			points += 1
-	if position.y > get_viewport_rect().end.y:
-		print("Died")
-		lives -= 1
-		position.y = 480
-		position.x = 512
-
-		
+ if position.y > get_viewport_rect().end.y:
+  emit_signal("lives")
+  queue_free()
